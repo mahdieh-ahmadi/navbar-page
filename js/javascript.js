005c2d566cfef306d968c5faba6a4b90b2fe7df5
mahdie <m119.ahmadi@gmail.com>
2020-09-13 20:52:48 +0430

    initial-folders

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+document.querySelector('.hamberger-meno').addEventListener('click' , 
+() =>{
+     document.querySelector('.hamberger-meno').classList.toggle('change');
+     document.querySelector('.hover').classList.toggle('change');
+     document.querySelector('.hover2').classList.toggle('change');
+});
